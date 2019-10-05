e47a5c6a6d795c2fb75ad05a06ec57217aa462ce
appunara <appunara@gmail.com>
2019-10-05 19:11:25 +0530

    added header section on all pages with logo link to home and shop link to shop page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.conponent';
 
 const HatsPage = () => (
   <div>
@@ -33,6 +34,7 @@ const MenPage = () => (
 function App() {
   return (
     <div>  
+    <Header/>
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route exact path="/shop" component={ShopPage}/>
